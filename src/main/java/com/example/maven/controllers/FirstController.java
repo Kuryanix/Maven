0c21289cc8eda6bf7343c27d433aaf59bc64b705
Максим <99maks@bk.ru>
2022-12-15 17:27:17 +0300

    ДЗ

---
+++ src/main/java/com/example/maven/controllers/FirstController.java
@@ -0,0 +1,21 @@
+package com.example.maven.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Приложение запущено";
+    }
+
+    @GetMapping("/info")
+    public String information() {
+        return "Имя ученика: " + "Курьян Максим" + ", "
+                + "Название проекта: "+"Maven"+", "
+                + "Дата создания проекта: "+"15.12.2022"+", "
+                + "Описание проекта: "+"тренировка работы c Maven";
+    }
+}
